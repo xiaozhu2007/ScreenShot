d9d587e361d5d31c294357f6060d7f2a3907746b
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-06-18 23:05:37 +0800

    Retry

---
+++ README.md
@@ -0,0 +1,14 @@
+# 自动截图
+
+向<https://github.com/xiaozhu2007/ScreenShot/blob/master/index.js>按照格式提交PR
+
+添加一行
+
+```javascript
+snap("https://<YOUR DOMAIN>", "./snap/<DOMAIN NAME>.png");
+```
+
+PR状态: **允许** 添加截图请求
+
+默认大小1024\*655
+可在<https://cdn.jsdelivr.net/gh/xiaozhu2007/ScreenShot@master/>看到.
--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const puppeteer = require('puppeteer');
-const fs = require("fs");
+const fs = require('fs');
+const ora  = require('ora');
+const fs = require('fs'); 
+const path = require('path');
+const chalk = require('chalk');
+const spinner = ora('Loading puppeteer...').start();
 
 const isDebugger = false;
 
-async function snap(url, img) {
+async function snap(url, save_to, save_pdf_to = save_to) {
     const browser = await puppeteer.launch({
         // 无头模式，不打开浏览器显示脚本运行过程，可以在调试过程中打开
         headless: true,
         // 设置浏览器窗口大小
         defaultViewport: {
-          width: 1000,
-          height: 2000,
+          width: 1024,
+          height: 655,
         }
       });
     const page = await browser.newPage();
     try {
         // 进入登陆页面，并等待直到没有网络连接的时候向下进行
         await page.goto(url, {
-          waitUntil: "networkidle2",
+          waitUntil: 'networkidle2',
         });
     } catch(e) {
-        console.log(`[Error] 原因: ${e}`);
+        console.log(`[Error] 错误原因: ${e}`);
         // 关闭浏览器并返回不再向下运行，本次Fetch失败
         await browser.close();
         return;
     }
     // page.once('load', () => console.log('Page loaded!'));
-    await page.screenshot({ path: img, fullPage: true});
+    setTimeout(() => {
+        spinner.text = `Saving to png (path: ${save_to})...`;
+    }, 1000);
+    await page.screenshot({ path: save_to, fullPage: true});
+    setTimeout(() => {
+        spinner.text = `Saving to pdf (path: ${save_pdf_to})...`;
+    }, 1000);
+    await page.pdf({
+        path: save_pdf_to,
+        format: 'a4',
+      });
     await browser.close();
 }
 
-snap("https://www.baidu.com", "./snap/baidu.png");
+snap('https://www.baidu.com', './snap/baidu.com.png', './pdf/baidu.com.pdf');
