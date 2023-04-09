26288a8dd067458878072f182f7251ac4b0e86a6
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-04-09 21:33:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ async function snap(url, save_to, save_pdf_to = save_to) {
     const page = await browser.newPage();
 
     try {
-        // 进入登陆页面，并等待直到没有网络连接的时候向下进行
+        // 进入页面，并等待直到没有网络连接的时候向下进行
         await page.goto(url, {
           waitUntil: 'networkidle2',
         });
     } catch(e) {
         console.log(`[Error] 错误原因: ${e}`);
-        // 关闭浏览器并返回不再向下运行，本次Fetch失败
+        // 关闭浏览器并返回不再向下运行，本次截图失败
         await browser.close();
         return;
     }
@@ -45,24 +45,22 @@ async function snap(url, save_to, save_pdf_to = save_to) {
 
 /**
  * 请先阅读以下内容再提交PR，否则直接关闭！
- * 1. 我的PR是为了添加链接
- * 2. 我同意Github Community Guideline、JSDelivrEULA、
- *  甜力怕's 隐私政策 和 Chromium的许可证(BSD)
- * 3. 我愿意使用Fastgit.org 和 Github Pages作为内容分发
- * 4. 拒绝反动\暴力\色情\宗教\伦理类信息,最好不要推销V·P·N，支持非首页（您可以随时更改），支持但不建议JavaScript动态加载(未来会禁用)
- * 注意：在您更改后，如果出现反动\暴力\色情\宗教\伦理类信息，则立刻删除，并不给予告知！
+ * 1. 我的 PR 是为了添加链接
+ * 2. 我同意 Github Community Guideline & JSDelivr EULA &
+ *  甜力怕's 隐私政策 和 Chromium 的许可证(BSD)
+ * 3. 我愿意使用 JSDelivr 和 HelloToolsCloud 作为内容分发
+ * 4. 拒绝反动\暴力\色情\宗教\伦理类信息,支持非首页,支持但不建议 JavaScript 动态加载
+ * 注意：在您更改后, 如果出现反动\暴力\色情\宗教\伦理类信息,则立刻删除!
  */
 // 万能百度
 snap('https://www.baidu.com', './snap/baidu.com.png', './pdf/baidu.com.pdf');
-// Pig2333's Blog
+// HackPig520's Blog
 snap('https://xiaozhu2007.netlify.app/', './snap/xiaozhu2007-blog-netlify.png', './pdf/xiaozhu2007-blog-netlify.pdf');
-// 小猪博客
-snap('https://xzblog.vercel.app/', './snap/xiaozhu2007-blog-vercel.png', './pdf/xiaozhu2007-blog-vercel.pdf');
-// 小猪cnblogs
+// HackPig520 博客园
 snap('https://cnblogs.com/xiaozhu2020/', './snap/xiaozhu2007-blog-cnblogs.png', './pdf/xiaozhu2007-blog-cnblogs.pdf');
 // ChenYFan Blog
 snap('https://blog.cyfan.top/', './snap/cyfan-blog.png', './pdf/cyfan-blog.pdf');
-// 百度实时热搜榜
+// 百度热搜榜
 snap('https://top.baidu.com/board?tab=realtime', './snap/baidu-top.png', './pdf/baidu-top.pdf');
 // Google News - US
 snap('https://news.google.com/home?hl=en-US&gl=US&ceid=US%3Aen&v2prv=1', './snap/google-news-us.png', './pdf/google-news-us.pdf');
